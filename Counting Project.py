16fb84e825a1420ee3e02a3e5fde5002372d4e07
kingmayonaise <danielmelnikov@hanovernorwichschools.org>
2017-05-05 13:51:50 -0400

    Updated from Brython Server: 5/5/2017 1:51:50 PM

--- Counting Project.py
+++ Counting Project.py
@@ -1,1 +1,20 @@
+from random import randint,shuffle
+import random
 
+trees=['M','M','M','O','O','O','O','B','B','B','B','B']
+
+failures=0
+trials=100
+
+for i in range(trials):
+    
+    
+    shuffle(trees)
+    #print(trees)
+    for j in range(9):
+        if trees[j]+trees[j+1]+trees[j+2]+trees[j+3]=='BBBB':
+     #       print("failure")
+            failures+=1
+            break
+        
+print((trials-failures)/trials*100,'%')
