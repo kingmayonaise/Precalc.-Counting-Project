49c7d2c7174a15e78eba03240b0e362aaa47eef4
kingmayonaise <danielmelnikov@hanovernorwichschools.org>
2017-05-08 17:42:37 -0400

    Updated from Brython Server: 5/8/2017 5:42:35 PM

--- Counting Project.py
+++ Counting Project.py
@@ -1,1 +1,20 @@
-print("Hello, world.")
+from random import randint,shuffle
+import random
+
+cards=['K','K','K','K','Q','Q','Q','Q','J','J','J','J']
+
+failures=0
+trials=10
+
+for i in range(trials):
+    
+    
+    shuffle(cards)
+    print(cards)
+    for j in range(4):
+        if cards[j]+cards[j+1]+cards[j+2]+cards[j+3]=='KKKK'or'QQQQ'or'JJJJ':
+            print('failure')
+            #failures+=1
+            #break
+        
+#print((trials-failures)/trials*100,'%')
