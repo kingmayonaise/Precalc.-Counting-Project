3b34cfb072e7f9f7648a0ae2c370a7da6f791fa5
kingmayonaise <danielmelnikov@hanovernorwichschools.org>
2017-05-09 17:04:21 -0400

    Updated from Brython Server: 5/9/2017 5:04:20 PM

--- Counting Project.py
+++ Counting Project.py
@@ -11,10 +11,29 @@ for i in range(trials):
     
     shuffle(cards)
     cards_concat=''.join(cards)
-    print(cards)
+ #   print(cards)
     if ('KKKK' in cards_concat) or \
             ('JJJJ' in cards_concat) or\
             ('QQQQ' in cards_concat):
+            success+=1
+            print('success: '+str(success))
+
+#print ("successes: "+str(success))        
+#print((trials-success)/trials*100,'%')
+
+
+kidsandteachers=['K','K','K','K','K','K','K','K','K','K','K','K','T','T','T']
+
+success=0
+trials=1000
+
+for i in range(trials):
+    
+    
+    shuffle(kidsandteachers)
+    kandt_concat=''.join(kidsandteachers)
+    print(kandt_concat)
+    if ('KTK' in kandt_concat):
             success+=1
             print('success: '+str(success))
 
