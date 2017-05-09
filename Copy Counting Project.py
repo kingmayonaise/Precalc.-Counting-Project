9237d3565716f532741ec5e833fbb65dedfe4bea
kingmayonaise <danielmelnikov@hanovernorwichschools.org>
2017-05-09 18:28:03 -0400

    Updated from Brython Server: 5/9/2017 6:28:02 PM

--- Copy Counting Project.py
+++ Copy Counting Project.py
@@ -1,14 +1,18 @@
 from random import randint,shuffle
 import random
 
+kidsandteachers=['K','K','K','K','K','K','K','K','K','K','K','K','T','T','T']
 
+success=0
+trials=10
+print(len(kidsandteachers[0]+kidsandteachers[1]+kidsandteachers[2]))
 for i in range(trials):
     
     
     shuffle(kidsandteachers)
-    kandt_concat=''.join(kidsandteachers)
-    print(kandt_concat)
-    if kandt_concat.count('KTK')==3:
+    print(kidsandteachers)
+    for j in range(13):
+        if (len(kidsandteachers[j]+kidsandteachers[j+1]+kidsandteachers[j+2]=='KTK')==9):
             success+=1
             print('success: '+str(success))
 
