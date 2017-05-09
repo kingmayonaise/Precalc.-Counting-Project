ff55e95a7c3d1d5a45b0c663a59d78e65227f55e
kingmayonaise <danielmelnikov@hanovernorwichschools.org>
2017-05-09 19:13:46 -0400

    Updated from Brython Server: 5/9/2017 7:13:45 PM

--- Counting Project.py
+++ Counting Project.py
@@ -26,16 +26,20 @@ kidsandteachers=['K','K','K','K','K','K','K','K','K','K','K','K','T','T','T']
 
 success=0
 trials=10000
-
+print(len(kidsandteachers[0]+kidsandteachers[1]+kidsandteachers[2]))
 for i in range(trials):
     
     
     shuffle(kidsandteachers)
-    kandt_concat=''.join(kidsandteachers)
-    print(kandt_concat)
-    if kandt_concat.count('KTK')==3:
-            success+=1
-            print('success: '+str(success))
+    print(kidsandteachers)
+    numberofKTK=0
+    for j in range(13):
+        if (kidsandteachers[j]+kidsandteachers[j+1]+kidsandteachers[j+2])=='KTK':
+            numberofKTK+=1
+            if numberofKTK==3:
+                numberofKTK=0
+                success+=1
+                print('success: '+str(success))
 
 print ("successes: "+str(success))        
 print((success)/trials*100,'%')
