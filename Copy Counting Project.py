38ccd3163b00f3eeb3e2a5f046eee2290b1ca227
kingmayonaise <danielmelnikov@hanovernorwichschools.org>
2017-05-09 18:39:08 -0400

    Updated from Brython Server: 5/9/2017 6:39:07 PM

--- Copy Counting Project.py
+++ Copy Counting Project.py
@@ -4,17 +4,20 @@ import random
 kidsandteachers=['K','K','K','K','K','K','K','K','K','K','K','K','T','T','T']
 
 success=0
-trials=10
+trials=10000
 print(len(kidsandteachers[0]+kidsandteachers[1]+kidsandteachers[2]))
 for i in range(trials):
     
     
     shuffle(kidsandteachers)
     print(kidsandteachers)
+    numberofKTK=0
     for j in range(13):
-        if (len(kidsandteachers[j]+kidsandteachers[j+1]+kidsandteachers[j+2]=='KTK')==9):
-            success+=1
-            print('success: '+str(success))
+        if (kidsandteachers[j]+kidsandteachers[j+1]+kidsandteachers[j+2])=='KTK':
+            numberofKTK+=1
+            if numberofKTK==3:
+                success+=1
+                print('success: '+str(success))
 
 print ("successes: "+str(success))        
 print((success)/trials*100,'%')
