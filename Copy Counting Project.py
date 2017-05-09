6887abd5f7b8e6fb56193432281b1857d5c4503e
kingmayonaise <danielmelnikov@hanovernorwichschools.org>
2017-05-09 18:10:31 -0400

    Updated from Brython Server: 5/9/2017 6:10:30 PM

--- Copy Counting Project.py
+++ Copy Counting Project.py
@@ -1,31 +1,6 @@
 from random import randint,shuffle
 import random
 
-cards=['K','K','K','K','Q','Q','Q','Q','J','J','J','J']
-
-success=0
-trials=1000
-
-for i in range(trials):
-    
-    
-    shuffle(cards)
-    cards_concat=''.join(cards)
- #   print(cards)
-    if ('KKKK' in cards_concat) or \
-            ('JJJJ' in cards_concat) or\
-            ('QQQQ' in cards_concat):
-            success+=1
-            print('success: '+str(success))
-
-#print ("successes: "+str(success))        
-#print((trials-success)/trials*100,'%')
-
-
-kidsandteachers=['K','K','K','K','K','K','K','K','K','K','K','K','T','T','T']
-
-success=0
-trials=10000
 
 for i in range(trials):
     
