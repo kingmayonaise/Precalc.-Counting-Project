8120b691011a301cd0a383a5731cf6a896d44326
kingmayonaise <danielmelnikov@hanovernorwichschools.org>
2017-05-10 14:43:28 -0400

    Update Counting Project.py

--- Counting Project.py
+++ Counting Project.py
@@ -4,19 +4,19 @@ import random
 cards=['K','K','K','K','Q','Q','Q','Q','J','J','J','J']
 
 success=0
-trials=1000
+trials=10000
 
 for i in range(trials):
     
     
     shuffle(cards)
     cards_concat=''.join(cards)
- #   print(cards)
+  #  print(cards)
     if ('KKKK' in cards_concat) or \
             ('JJJJ' in cards_concat) or\
             ('QQQQ' in cards_concat):
             success+=1
-            print('success: '+str(success))
+#            print('success: '+str(success))
 
 #print ("successes: "+str(success))        
 #print((trials-success)/trials*100,'%')
@@ -26,20 +26,37 @@ kidsandteachers=['K','K','K','K','K','K','K','K','K','K','K','K','T','T','T']
 
 success=0
 trials=10000
-print(len(kidsandteachers[0]+kidsandteachers[1]+kidsandteachers[2]))
 for i in range(trials):
     
     
     shuffle(kidsandteachers)
-    print(kidsandteachers)
+  #  print(kidsandteachers)
     numberofKTK=0
-    for j in range(13):
-        if (kidsandteachers[j]+kidsandteachers[j+1]+kidsandteachers[j+2])=='KTK':
+    for j in range(11):
+        if (kidsandteachers[j]+kidsandteachers[j+1]+kidsandteachers[j+2]+kidsandteachers[j+3]+kidsandteachers[j+4])=='KKTKK':
             numberofKTK+=1
             if numberofKTK==3:
                 numberofKTK=0
                 success+=1
-                print('success: '+str(success))
+ #               print('success: '+str(success))
 
-print ("successes: "+str(success))        
-print((success)/trials*100,'%')
+#print ("successes: "+str(success))        
+#print((success)/trials*100,'%')
+
+
+pairs=[1,1,2,2,3,3,4,4,5,5]
+
+
+success=0
+trials=10
+for i in range(trials):
+    
+    shuffle(pairs)
+    print(pairs)
+    for j in range(8):
+        print(str(pairs[j])+str(pairs[j+1]))
+    
+    
+    
+    
+    
