79553411bddba1c1aa3c82ddd3a45c8b5d298f95
kingmayonaise <danielmelnikov@hanovernorwichschools.org>
2017-05-14 15:53:49 -0400

    Update Counting Project.py

--- Counting Project.py
+++ Counting Project.py
@@ -20,7 +20,7 @@ for i in range(trials):
 
 #print ("successes: "+str(success))        
 #print((trials-success)/trials*100,'%')
-"""
+
 
 kidsandteachers=['K','K','K','K','K','K','K','K','K','K','K','K','T','T','T']
 
@@ -42,21 +42,41 @@ for i in range(trials):
 
 print ("successes: "+str(success))        
 print((success)/trials*100,'%')
-
 """
+
 pairs=[1,1,2,2,3,3,4,4,5,5]
 
 
 success=0
 trials=10
+paircount=0
+onepair=0
+twopair=0
+threepair=0
+fourpair=0
+fivepair=0
 for i in range(trials):
-    
+    paircount=0
     shuffle(pairs)
-   # print(pairs)
+    print(pairs)
     for j in range(8):
         if (int(str(pairs[j])+str(pairs[j+1]))%11)==0:
-         #   print('pair')
-    """
+           paircount+=1
+           print('pair')
+    if paircount==1:
+        onepair+=1
+    elif paircount==2:
+        twopair+=1
+    elif paircount==3:
+        threepair+=1
+    elif paircount==4:
+        fourpair+=1
+    elif paircount==5:
+        fivepair+=1
+print(onepair,twopair,threepair,fourpair,fivepair)
+print((onepair*((22680/1134000)*1))+(twopair*((5040/1134000)*2))+(threepair*((1260/1134000)*3))+(fourpair*((360/1134000)*4))+(fivepair*((120/1134000)*5)))
+
+
     
     
     
