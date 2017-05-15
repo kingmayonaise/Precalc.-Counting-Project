65a163092555cc14143620f870dbba4313f76363
kingmayonaise <danielmelnikov@hanovernorwichschools.org>
2017-05-15 18:34:35 -0400

    Updated from Brython Server: 5/15/2017 6:34:34 PM

--- Counting Project.py
+++ Counting Project.py
@@ -49,25 +49,32 @@ pairs=[1,1,2,2,3,3,4,4,5,5]
 
 success=0
 trials=10000
+onecounter=0
+totalcounter=0
 paircounters=[0,0,0,0,0]
 for i in range(trials):
     paircount=0
     shuffle(pairs)
     print(pairs)
-    for j in range(8):
+    for j in range(9):
         if (int(str(pairs[j])+str(pairs[j+1]))%11)==0:
            paircount+=1
+           totalcounter+=1
+        if (int(str(pairs[j])+str(pairs[j+1])))==22:
+           onecounter+=1
            
     if paircount>0:
         print(str(paircount)+' pairs')
         paircounters[paircount-1]+=1
 print(paircounters)
 print(((paircounters[0]/trials)*1)+((paircounters[1]/trials)*2)+((paircounters[2]/trials)*3)+((paircounters[3]/trials)*4)+((paircounters[4]/trials)*5))
-print (paircounters[0]/trials)
-print (paircounters[1]/trials)
-print (paircounters[2]/trials)
-print (paircounters[3]/trials)
-print (paircounters[4]/trials)
+print ('1 pairs '+str(paircounters[0]/trials))
+print ('2 pairs '+str(paircounters[1]/trials))
+print ('3 pairs '+str(paircounters[2]/trials))
+print ('4 pairs '+str(paircounters[3]/trials))
+print ('5 pairs '+str(paircounters[4]/trials))
+print(onecounter/trials)
+print ('Total '+str(totalcounter))
 
 
     
